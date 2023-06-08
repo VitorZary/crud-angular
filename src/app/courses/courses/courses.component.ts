4fe408505b36ca2313e70332680a5fe08a199ef1
Vitor Hugo Kuhn Zary <vitorhbts@.com>
2023-06-08 19:37:17 -0300

    pt 1 projeto

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CoursesService } from '../services/courses.service';
+import { Course } from './../model/course';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit{
+
+  //coursesService: CoursesService;
+
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category', 'actions']
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+    ){
+    // this.courses = [];
+    //this.coursesService = new CoursesService();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.')
+        return of([])
+      })
+    );
+
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onAdd(event?: MouseEvent){
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+}
